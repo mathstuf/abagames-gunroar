472eea7d1af16031865d1c91f2a05affa6474dec
Ben Boeckel <mathstuf@gmail.com>
2014-04-11 16:17:00 -0400

    Force landscape orientation on Android

--- src/android/src/net/benboeckel/abagames/gunroar/GunroarActivity.java
+++ src/android/src/net/benboeckel/abagames/gunroar/GunroarActivity.java
@@ -4,4 +4,10 @@ import org.libsdl.app.SDLActivity;
 
 public class GunroarActivity extends SDLActivity
 {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+    }
 }
