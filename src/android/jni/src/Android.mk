01d79aca4444b52ce7138bec5a3396ce4a99dcce
Ben Boeckel <mathstuf@gmail.com>
2014-04-14 23:56:02 -0400

    android: Remove useless comment

--- src/android/jni/src/Android.mk
+++ src/android/jni/src/Android.mk
@@ -9,7 +9,6 @@ SDL_MIXER_PATH := ../SDL_mixer
 
 LOCAL_C_INCLUDES := $(SDL_PATH)/include $(SDL_MIXER_PATH)/include
 
-# Add your application source files here...
 LOCAL_SRC_FILES := SDL_android_main.c
 
 LOCAL_SHARED_LIBRARIES := SDL2 SDL2_mixer gunroar_main
