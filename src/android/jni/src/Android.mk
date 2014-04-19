928245e62ff01c9837a31cf29522be332c22d958
Ben Boeckel <mathstuf@gmail.com>
2014-04-19 03:14:15 -0400

    android: Clean up the shim's build file

--- src/android/jni/src/Android.mk
+++ src/android/jni/src/Android.mk
@@ -5,14 +5,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := main
 
 SDL_PATH := ../SDL
-SDL_MIXER_PATH := ../SDL_mixer
 
-LOCAL_C_INCLUDES := $(SDL_PATH)/include $(SDL_MIXER_PATH)/include
-
-LOCAL_SRC_FILES := SDL_android_main.c
-
+LOCAL_C_INCLUDES       := $(SDL_PATH)/include
+LOCAL_SRC_FILES        := SDL_android_main.c
 LOCAL_SHARED_LIBRARIES := SDL2 SDL2_mixer gunroar_main
-
-LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2
 
 include $(BUILD_SHARED_LIBRARY)
