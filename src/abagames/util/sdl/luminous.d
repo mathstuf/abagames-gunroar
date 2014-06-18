c305eb443d0783ab02d4fb34e4ee912961cbbbc8
Ben Boeckel <mathstuf@gmail.com>
2014-06-17 22:52:01 -0400

    luminous: port to gles-compatible calls

--- src/abagames/util/sdl/luminous.d
+++ src/abagames/util/sdl/luminous.d
@@ -7,9 +7,11 @@ module abagames.util.sdl.luminous;
 
 private import std.math;
 private import std.c.string;
+private import derelict.opengl3.arb;
 private import derelict.opengl3.gl;
 private import gl3n.linalg;
 private import abagames.util.actor;
+private import abagames.util.sdl.shaderprogram;
 
 /**
  * Luminous effect texture.
@@ -25,10 +27,128 @@ public class LuminousScreen {
   int luminousTextureWidth = 64, luminousTextureHeight = 64;
   int screenWidth, screenHeight;
   float luminosity;
+  ShaderProgram program;
+  GLuint[2] vao;
+  GLuint[4] vbo;
+
+  //private int lmOfs[5][2] = [[0, 0], [1, 0], [-1, 0], [0, 1], [0, -1]];
+  //private const float lmOfsBs = 5;
+  //private static float lmOfs[2][2] = [[-2, -1], [2, 1]];
+  private static const float lmOfs_a0 = -2;
+  private static const float lmOfs_a1 = -1;
+  private static const float lmOfs_b0 =  2;
+  private static const float lmOfs_b1 =  1;
+  private static const float lmOfsBs = 3;
 
   public void init(float luminosity, int width, int height) {
     makeLuminousTexture();
     this.luminosity = luminosity;
+
+    program = new ShaderProgram;
+    program.setVertexShader(
+      "uniform mat4 projmat;\n"
+      "uniform float luminousFactor;\n"
+      "\n"
+      "attribute vec2 diff;\n"
+      "attribute vec2 pos;\n"
+      "attribute vec2 tex;\n"
+      "\n"
+      "varying vec2 f_tc;\n"
+      "\n"
+      "void main() {\n"
+      "  gl_Position = projmat * vec4(diff + pos * luminousFactor, 0, 1);\n"
+      "  f_tc = tex;\n"
+      "}\n"
+    );
+    program.setFragmentShader(
+      "uniform sampler2D sampler;\n"
+      "uniform vec4 color;\n"
+      "\n"
+      "varying vec2 f_tc;\n"
+      "\n"
+      "void main() {\n"
+      "  gl_FragColor = texture2D(sampler, f_tc) * color;\n"
+      "}\n"
+    );
+    GLint diffLoc = 0;
+    GLint posLoc = 1;
+    GLint texLoc = 2;
+    program.bindAttribLocation(diffLoc, "diff");
+    program.bindAttribLocation(posLoc, "pos");
+    program.bindAttribLocation(texLoc, "tex");
+    program.link();
+    program.use();
+
+    program.setUniform("color", 1, 0.8, 0.9, luminosity);
+    program.setUniform("sampler", 0);
+    program.setUniform("luminousFactor", lmOfsBs);
+
+    glGenBuffers(4, vbo.ptr);
+    glGenVertexArrays(2, vao.ptr);
+
+    static const float[] VTX1 = [
+      lmOfs_a0, lmOfs_a1,
+      lmOfs_a0, lmOfs_a1,
+      lmOfs_a0, lmOfs_a0,
+      lmOfs_a0, lmOfs_a0
+    ];
+    static const float[] VTX2 = [
+      lmOfs_b0, lmOfs_b1,
+      lmOfs_b0, lmOfs_b1,
+      lmOfs_b0, lmOfs_b0,
+      lmOfs_b0, lmOfs_b0
+    ];
+    static const float[] TEXTURE = [
+      TEXTURE_SIZE_MIN, TEXTURE_SIZE_MAX,
+      TEXTURE_SIZE_MIN, TEXTURE_SIZE_MIN,
+      TEXTURE_SIZE_MAX, TEXTURE_SIZE_MIN,
+      TEXTURE_SIZE_MAX, TEXTURE_SIZE_MAX
+    ];
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+
+    glBindVertexArray(vao[0]);
+
+    glVertexAttribPointer(diffLoc, 2, GL_FLOAT, GL_FALSE, 0, null);
+    glEnableVertexAttribArray(diffLoc);
+
+    glBindVertexArray(vao[1]);
+
+    glVertexAttribPointer(diffLoc, 2, GL_FLOAT, GL_FALSE, 0, null);
+    glEnableVertexAttribArray(diffLoc);
+
+    glBindVertexArray(vao[0]);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+    glBufferData(GL_ARRAY_BUFFER, VTX1.length * float.sizeof, VTX1.ptr, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(posLoc, 2, GL_FLOAT, GL_FALSE, 0, null);
+    glEnableVertexAttribArray(posLoc);
+
+    glBindVertexArray(vao[1]);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[2]);
+    glBufferData(GL_ARRAY_BUFFER, VTX2.length * float.sizeof, VTX2.ptr, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(posLoc, 2, GL_FLOAT, GL_FALSE, 0, null);
+    glEnableVertexAttribArray(posLoc);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[3]);
+    glBufferData(GL_ARRAY_BUFFER, TEXTURE.length * float.sizeof, TEXTURE.ptr, GL_STATIC_DRAW);
+
+    glBindVertexArray(vao[0]);
+
+    glVertexAttribPointer(texLoc, 2, GL_FLOAT, GL_FALSE, 0, null);
+    glEnableVertexAttribArray(texLoc);
+
+    glBindVertexArray(vao[1]);
+
+    glVertexAttribPointer(texLoc, 2, GL_FLOAT, GL_FALSE, 0, null);
+    glEnableVertexAttribArray(texLoc);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
     resized(width, height);
   }
 
@@ -47,10 +167,15 @@ public class LuminousScreen {
   public void resized(int width, int height) {
     screenWidth = width;
     screenHeight = height;
+
+    loadScreenData();
   }
 
   public void close() {
     glDeleteTextures(1, &luminousTexture);
+    glDeleteVertexArrays(2, vao.ptr);
+    glDeleteBuffers(4, vbo.ptr);
+    program.close();
   }
 
   public void startRender() {
@@ -64,49 +189,43 @@ public class LuminousScreen {
     glViewport(0, 0, screenWidth, screenHeight);
   }
 
-  private mat4 viewOrtho() {
-    glMatrixMode(GL_PROJECTION);
-    glPushMatrix();
-    glLoadIdentity();
-    glOrtho(0, screenWidth, screenHeight, 0, -1, 1);
-    glMatrixMode(GL_MODELVIEW);
-    glPushMatrix();
-    glLoadIdentity();
-    return mat4.orthographic(0, screenWidth, screenHeight, 0, -1, 1);
-  }
+  private void loadScreenData() {
+    program.use();
 
-  private void viewPerspective() {
-    glMatrixMode(GL_PROJECTION);
-    glPopMatrix();
-    glMatrixMode(GL_MODELVIEW);
-    glPopMatrix();
-  }
+    mat4 view = mat4.orthographic(0, screenWidth, screenHeight, 0, -1, 1);
+    program.setUniform("projmat", view);
 
-  //private int lmOfs[5][2] = [[0, 0], [1, 0], [-1, 0], [0, 1], [0, -1]];
-  //private const float lmOfsBs = 5;
-  private static const float lmOfs[2][2] = [[-2, -1], [2, 1]];
-  private static const float lmOfsBs = 3;
+    const float[] DIFF = [
+      0,           0,
+      0,           screenHeight,
+      screenWidth, screenHeight,
+      screenWidth, 0
+    ];
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glBufferData(GL_ARRAY_BUFFER, DIFF.length * float.sizeof, DIFF.ptr, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+  }
 
   public void draw(mat4 /*view*/) {
     glEnable(GL_TEXTURE_2D);
+
+    program.use();
+
+    glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, luminousTexture);
-    mat4 view = viewOrtho();
-    glColor4f(1, 0.8, 0.9, luminosity);
-    glBegin(GL_QUADS);
-    //for (int i = 0; i < 5; i++) {
-    //for (int i = 1; i < 5; i++) {
-    for (int i = 0; i < 2; i++) {
-      glTexCoord2f(TEXTURE_SIZE_MIN, TEXTURE_SIZE_MAX);
-      glVertex2f(0 + lmOfs[i][0] * lmOfsBs, 0 + lmOfs[i][1] * lmOfsBs);
-      glTexCoord2f(TEXTURE_SIZE_MIN, TEXTURE_SIZE_MIN);
-      glVertex2f(0 + lmOfs[i][0] * lmOfsBs, screenHeight + lmOfs[i][1] * lmOfsBs);
-      glTexCoord2f(TEXTURE_SIZE_MAX, TEXTURE_SIZE_MIN);
-      glVertex2f(screenWidth + lmOfs[i][0] * lmOfsBs, screenHeight + lmOfs[i][0] * lmOfsBs);
-      glTexCoord2f(TEXTURE_SIZE_MAX, TEXTURE_SIZE_MAX);
-      glVertex2f(screenWidth + lmOfs[i][0] * lmOfsBs, 0 + lmOfs[i][0] * lmOfsBs);
-    }
-    glEnd();
-    viewPerspective();
+
+    glBindVertexArray(vao[0]);
+    glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+
+    glBindVertexArray(vao[1]);
+    glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+
+    glBindVertexArray(0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glUseProgram(0);
+
     glDisable(GL_TEXTURE_2D);
   }
 }
