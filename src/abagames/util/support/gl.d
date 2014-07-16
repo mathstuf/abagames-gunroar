a723c3f32af3493c6f350725f25dd6551136ee0e
Ben Boeckel <mathstuf@gmail.com>
2014-07-16 00:21:42 -0400

    whitespace: use 2 space indents

--- src/abagames/util/support/gl.d
+++ src/abagames/util/support/gl.d
@@ -6,48 +6,48 @@
 module abagames.util.support.gl;
 
 version (Android) {
-    private import derelict.gles.egl;
-    public import derelict.gles.gles2;
-    private import derelict.gles.ext2;
+  private import derelict.gles.egl;
+  public import derelict.gles.gles2;
+  private import derelict.gles.ext2;
 
-    public enum usingGLES = true;
+  public enum usingGLES = true;
 } else {
-    public import derelict.opengl3.gl;
+  public import derelict.opengl3.gl;
 
-    public enum usingGLES = false;
+  public enum usingGLES = false;
 }
 
 public void loadGL() {
-    version (Android) {
-        DerelictEGL.load();
-        DerelictGLES2.load();
-    } else {
-        DerelictGL3.load();
-    }
+  version (Android) {
+    DerelictEGL.load();
+    DerelictGLES2.load();
+  } else {
+    DerelictGL3.load();
+  }
 }
 
 public void reloadGL() {
-    version (Android) {
-        DerelictGLES2.reload();
+  version (Android) {
+    DerelictGLES2.reload();
 
-        if (!GL_OES_vertex_array_object) {
-            throw new Exception("error: required GLES extension GL_OES_vertex_array_object not supported");
-        }
-    } else {
-        DerelictGL3.reload();
+    if (!GL_OES_vertex_array_object) {
+      throw new Exception("error: required GLES extension GL_OES_vertex_array_object not supported");
     }
+  } else {
+    DerelictGL3.reload();
+  }
 }
 
 version (Android) {
-    public void glGenVertexArrays(GLsizei n, GLuint* arrays) {
-        glGenVertexArraysOES(n, arrays);
-    }
+  public void glGenVertexArrays(GLsizei n, GLuint* arrays) {
+    glGenVertexArraysOES(n, arrays);
+  }
 
-    public void glBindVertexArray(GLuint array) {
-        glBindVertexArrayOES(array);
-    }
+  public void glBindVertexArray(GLuint array) {
+    glBindVertexArrayOES(array);
+  }
 
-    public void glDeleteVertexArrays(GLsizei n, GLuint* arrays) {
-        glDeleteVertexArraysOES(n, arrays);
-    }
+  public void glDeleteVertexArrays(GLsizei n, GLuint* arrays) {
+    glDeleteVertexArraysOES(n, arrays);
+  }
 }
