9cad817b9055059a316eddc9af0bc41117cd5a31
Ben Boeckel <mathstuf@gmail.com>
2017-04-22 13:03:39 -0400

    turret: port the moving turret group spec class

--- src/game/entities/enemies/turret/mod.rs
+++ src/game/entities/enemies/turret/mod.rs
@@ -3,9 +3,13 @@
 
 mod normal;
 mod group;
+mod moving_group;
 
 pub use self::normal::TurretKind;
 pub use self::normal::TurretSpec;
 pub use self::group::TurretGroupAlignment;
 pub use self::group::TurretGroupSpec;
 pub use self::group::TurretGroupSpecBuilder;
+pub use self::moving_group::TurretMovement;
+pub use self::moving_group::MovingTurretGroupSpec;
+pub use self::moving_group::MovingTurretGroupSpecBuilder;
