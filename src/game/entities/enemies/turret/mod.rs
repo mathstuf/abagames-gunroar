198229011e4cc0042e40247b5143361030be0b42
Ben Boeckel <mathstuf@gmail.com>
2017-04-23 20:57:56 -0400

    turret/spec: move specs to a single file

--- src/game/entities/enemies/turret/mod.rs
+++ src/game/entities/enemies/turret/mod.rs
@@ -1,15 +1,13 @@
 // Distributed under the OSI-approved BSD 2-Clause License.
 // See accompanying file LICENSE for details.
 
-mod normal;
-mod group;
-mod moving_group;
+mod spec;
 
-pub use self::normal::TurretKind;
-pub use self::normal::TurretSpec;
-pub use self::group::TurretGroupAlignment;
-pub use self::group::TurretGroupSpec;
-pub use self::group::TurretGroupSpecBuilder;
-pub use self::moving_group::TurretMovement;
-pub use self::moving_group::MovingTurretGroupSpec;
-pub use self::moving_group::MovingTurretGroupSpecBuilder;
+pub use self::spec::MovingTurretGroupSpec;
+pub use self::spec::MovingTurretGroupSpecBuilder;
+pub use self::spec::TurretGroupAlignment;
+pub use self::spec::TurretGroupSpec;
+pub use self::spec::TurretGroupSpecBuilder;
+pub use self::spec::TurretKind;
+pub use self::spec::TurretMovement;
+pub use self::spec::TurretSpec;
